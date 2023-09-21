4e4dcd4106f4e5c84ef577118f3a059055f0030d
keegan <hengquan0204@gmail.com>
2023-09-21 18:44:50 +0800

    Add automated frontend testing on pull request for dev_frontend

--- frontend/chrono-quest/src/App.test.js
+++ frontend/chrono-quest/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App component', () => {
+    render(<App />);
+    const appTitle = screen.getByText('ChronoQuest');
+    expect(appTitle).toBeInTheDocument();
 });
+
+
