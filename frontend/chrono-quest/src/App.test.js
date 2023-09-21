d0e0ec7f63a7f0716053e06a9fcb309f3df6403d
keegan <hengquan0204@gmail.com>
2023-09-21 21:06:18 +0800

    Add test case in frontend testing

--- frontend/chrono-quest/src/App.test.js
+++ frontend/chrono-quest/src/App.test.js
@@ -8,22 +8,3 @@ test('renders App component', () => {
     expect(appTitle).toBeInTheDocument();
 });
 
-test('searching for a brand updates the search results', async () => {
-    render(<App />);
-
-    const searchInput = screen.getByPlaceholderText('Enter a watch brand to begin quest...');
-    const searchButton = screen.getByAltText('Watch Logo');
-
-    fireEvent.change(searchInput, { target: { value: 'Rolex' } });
-    fireEvent.click(searchButton);
-
-    // Use waitFor to wait for the search results to appear
-    await waitFor(() => {
-        const searchResults = screen.getByText('Search Results:');
-        expect(searchResults).toBeInTheDocument();
-    });
-
-    // Use separate expect statements to check individual conditions
-    const searchResultItem = screen.getByText('Rolex'); // Assuming 'Rolex' is in the search results
-    expect(searchResultItem).toBeInTheDocument();
-});
