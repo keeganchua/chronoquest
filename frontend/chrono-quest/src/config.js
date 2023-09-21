cfaa62f8642abaaaa43d129786f109f927cd6288
keegan <hengquan0204@gmail.com>
2023-09-21 23:45:51 +0800

    Changed API requests port to be flexible depending on environment

--- frontend/chrono-quest/src/config.js
+++ frontend/chrono-quest/src/config.js
@@ -1,13 +1,8 @@
-const config = {
-    development: {
-        apiUrl: 'http://localhost:3000', // Change this to your development API URL
-    },
-    production: {
-        apiUrl: 'https://your-production-api-url.com', // Change this to your production API URL
-    },
+const environments = {
+    development: require('./config.development'),
+    production: require('./config.production'),
 };
 
-// Determine the environment based on NODE_ENV
 const environment = process.env.NODE_ENV || 'development';
 
-export default config[environment];
+export default environments[environment];
