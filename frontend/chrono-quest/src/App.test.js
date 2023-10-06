ebccf9a23d7171b11495a2f9e41f385b3bd3b4e3
keegan <hengquan0204@gmail.com>
2023-10-07 00:26:01 +0800

    Added a test case

--- frontend/chrono-quest/src/App.test.js
+++ frontend/chrono-quest/src/App.test.js
@@ -8,3 +8,14 @@ test('renders App component', () => {
     expect(appTitle).toBeInTheDocument();
 });  
 
+test('renders App component with search input and button', () => {
+    render(<App />);
+    
+    // Check if the search input is present
+    const searchInput = screen.getByPlaceholderText('Enter a watch brand to begin quest....');
+    expect(searchInput).toBeInTheDocument();
+    
+    // Check if the search button is present (assuming the alt text is 'Watch Logo')
+    const searchButton = screen.getByAltText('Watch Logo');
+    expect(searchButton).toBeInTheDocument();
+});
