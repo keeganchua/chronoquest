d4a996e6b1dee231280b5f637aaa79f303649c3e
keegan <hengquan0204@gmail.com>
2023-10-16 16:29:36 +0800

    change backend production api url to 35.240.213.242:3000

--- frontend/chrono-quest/src/config.production.js
+++ frontend/chrono-quest/src/config.production.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: 'https://34.126.160.90:3000', // Change this to your production API URL
+    apiUrl: 'https://35.240.213.242:3000', // Change this to your production API URL
 };
 
 module.exports = config;
